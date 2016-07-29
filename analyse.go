4f4f72d1f41229b9870c1cba87d4384caa115de0
Phil Brookes <phil.brookes@redhat.com>
2016-07-29 10:51:02 +0100

    working on output

--- analyse.go
+++ analyse.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
-	"strconv"
+	"fmt"
 )
 
 func analyseTask() int {
-	checkForImagePullBackOff(*dumpFileLocation)
+	res, err := checkForImagePullBackOff(*dumpFileLocation)
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+
+	res.Output()
+
 	return 0
 }
 
 type checkResult struct {
 	Status  int
-	Message string
+	Info []checkInfo
 }
+
+func (c *checkResult) Output() {
+	if c.Status == 0 {
+		fmt.Println("Issue not detected.")
+		return
+	}
+
+	for _, item := range c.Info {
+		fmt.Println(item.ObjectName + " has ImagePullBackOff issue")
+	}
+}
+
+type checkInfo struct {
+	File string
+	Entry string
+	ObjectName string
+	Namespace string
+	Count int
+}
+
 
 type Events struct {
 	Kind       string `json:"kind"`
@@ -60,29 +86,30 @@ type Events struct {
 	} `json:"items"`
 }
 
-func checkForImagePullBackOff(logDir string) error {
-
+func checkForImagePullBackOff(logDir string) (*checkResult, error) {
+	result := &checkResult{Status: 0}
 	projectDirs, err := ioutil.ReadDir(filepath.Join(logDir, "projects"))
 	if err != nil {
-		return err
+		return result, err
 	}
 
 	for _, projectDir := range projectDirs {
 		events := Events{}
 		reader, err := os.Open(filepath.Join(logDir, "projects", projectDir.Name(), "events.json"))
 		if err != nil {
-			return err
+			return result, err
 		}
 		decoder := json.NewDecoder(reader)
 		decoder.Decode(&events)
 		for _, event := range events.Items {
 			if event.Reason == "FailedSync" && strings.Contains(event.Message, "ImagePullBackOff") {
-				fmt.Println(event.InvolvedObject.Name + " in project " + event.InvolvedObject.Namespace + " has failed to pull the image " + strconv.Itoa(event.Count) + "times.")
-				fmt.Println("Original Message: " + event.Message)
+				info := checkInfo{ObjectName: event.InvolvedObject.Name, Namespace: event.InvolvedObject.Namespace, Count: event.Count, Entry: event.Message}
+				result.Status = 1
+				result.Info = append(result.Info, info)
 			}
 		}
 	}
 
-	return nil
+	return result, nil
 
 }
